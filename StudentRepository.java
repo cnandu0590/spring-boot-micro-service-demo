38887a6ae5b2aa12fa8796f6ef2d67feff35cd34
mahanandi ch <nanduc999@gmail.com>
2020-01-19 16:07:15 -0800

    chnged ll

--- StudentRepository.java
+++ StudentRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sample.demo.entity.Course;
+import com.sample.demo.entity.ll;
 import com.sample.demo.entity.Student;
 
 @Repository
